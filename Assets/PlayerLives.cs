56389779ba6260ff9b2de508f0c05760484d4f76
Odoctopus <bratzia@hotmail.com>
2026-06-17 16:16:39 +0200

    work on level

---
+++ Assets/PlayerLives.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerLives : MonoBehaviour
+{
+    public int lives = 3;
+
+    private Vector2 startPosition;
+    private Vector2 checkpointPosition;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+        checkpointPosition = startPosition;
+    }
+
+    public void SetCheckpoint(Vector2 newCheckpoint)
+    {
+        checkpointPosition = newCheckpoint;
+    }
+
+    public void die()
+    {
+        lives--;
+
+        if (lives > 0)
+        {
+            transform.position = checkpointPosition;
+        }
+        else
+        {
+            lives = 1;
+            checkpointPosition = startPosition;
+            transform.position = startPosition;
+        }
+
+        Debug.Log("Lives: " + lives);
+    }
+}
