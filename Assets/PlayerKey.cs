6bfde01b2408fc6d640b6ff92f5b709cf67dfcbb
Odoctopus <bratzia@hotmail.com>
2026-06-26 11:43:53 +0200

    gdsf

--- Assets/PlayerKey.cs
+++ Assets/PlayerKey.cs
@@ -8,19 +8,33 @@ public class PlayerKey : MonoBehaviour
     private bool hasKey = false;
     private GameObject KeyObject;
 
-    public Vector3 keyOffset = new Vector3(0.7f, 0.5f, 0);
+    public Vector3 keyOffset = new Vector3(-0.1f, 0.1f, 0);
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Key"))
         {
             hasKey = true;
+            KeyObject = other.gameObject;
+
+            KeyObject.GetComponent<Collider2D>().enabled = false;
             Debug.Log("Key collected!");
         }
 }
 
     private void Update()
     {
+        if (hasKey && KeyObject != null)
+        {
+            Vector3 targetPos = transform.position + keyOffset;
+
+            KeyObject.transform.position = Vector3.Lerp(
+                KeyObject.transform.position,
+                targetPos,
+                6f * Time.deltaTime
+                );
+        }
+
         if (hasKey && Keyboard.current.eKey.wasPressedThisFrame)
         {
             wall.SetActive(false);
